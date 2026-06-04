56239c6915a6944b7482c580452de6cb49f08b6a
Chris Bunny <christopherjbunny@gmail.com>
2026-06-03 19:11:45 -0500

    Created multiple components for site

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+    return (
+        <section id="Contact">
+            <h1>Contact Me</h1>
+            <a href="https://www.linkedin.com/in/christopher-bunny-704617309/">LinkedIn</a>
+            <a href="https://github.com/CJB2003">GitHub</a>
+            <a href="mailto:christopherjbunny@gmail.com">Email me here</a>
+        </section>
+    )
+}
+export default Contact;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+    return (
+        <section id="hero">
+            <h1>Hi, I'm Chris Bunny</h1>
+            <h2>FullStack Developer.</h2>
+            <p>I'm currently an Software Development student.</p>
+        </section>
+    )
+}
+export default Hero;
