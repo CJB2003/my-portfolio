1d944033101ca486d340db43ce002f56d01b7fc0
Chris Bunny <christopherjbunny@gmail.com>
2026-06-04 20:11:12 -0500

    Completed my contacts section, may update with changes later down the road

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,12 +3,14 @@ import "../styling/Contact.css";
 function Contact() {
   return (
     <section id="Contact" className="contact">
-      <h1>Contact Me</h1>
-      <a href="https://www.linkedin.com/in/christopher-bunny-704617309/">
-        LinkedIn
-      </a>
-      <a href="https://github.com/CJB2003">GitHub</a>
-      <a href="mailto:christopherjbunny@gmail.com">Email me here</a>
+      <h1>Let's Connect</h1>
+      <div className="contact-links">
+        <a href="https://www.linkedin.com/in/christopher-bunny-704617309/">
+          LinkedIn
+        </a>
+        <a href="https://github.com/CJB2003">GitHub</a>
+        <a href="mailto:christopherjbunny@gmail.com">Email me here</a>
+      </div>
     </section>
   );
 }
